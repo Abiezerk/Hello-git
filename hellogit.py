5e3fc42822d17fde4883c03529597de9ec4d7982
Abiezerk <israelcanales95@gmail.com>
2023-07-26 09:48:58 -0700

    Este es el commit de navegacion

--- hellogit.py
+++ hellogit.py
@@ -1,1 +1,9 @@
-print('New hellogit acá perrón')
+print('''Bueno vamos a intentar mejor con este jeje
+      a ver y si por ejemplo le digo
+      enlistame estas cosas para las compras:
+      Bananas
+      Perejil
+      Huevo
+      Crema
+      Leche''')
+
